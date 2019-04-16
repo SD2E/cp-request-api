94efd33bdcccb90c29501f25513418ca565746ea
Ben Keller <bjkeller@uw.edu>
2019-04-16 14:58:00 -0700

    Add a version number

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cp_request',
+    version='0.0.1',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     install_requires=[]
