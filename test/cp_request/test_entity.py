dc073b6a7d5a672b75988cc4418367bfbef1aa20
Ben Keller <bjkeller@uw.edu>
2019-04-23 12:19:39 -0700

    Add tests for entity attributes

--- test/cp_request/test_entity.py
+++ test/cp_request/test_entity.py
@@ -23,11 +23,44 @@ class TestNamedEntity:
         assert e1 == e2
 
     def test_entity_attributes(self):
-        e1 = NamedEntity(name="one",
-                         reference="http://one.one",
-                         attributes=[
-                             Attribute.create_from(
-                                 name='concentration',
-                                 value=Value(value=0.25, unit=Unit(
-                                     reference='http://purl.obolibrary.org/obo/UO_0000064')))
-                         ])
+        concentration = Attribute.create_from(
+            name='concentration',
+            value=Value(
+                value=0.25,
+                unit=Unit(
+                    reference='http://purl.obolibrary.org/obo/UO_0000064'
+                )
+            ))
+        e1 = NamedEntity(
+            name="one",
+            reference="http://one.one",
+            attributes=[concentration])
+        e2 = NamedEntity(
+            name="one",
+            reference="http://one.one",
+            attributes=[concentration])
+        assert e1 == e1
+        assert e1 == e2
+        assert e1 != {}
+
+        assert repr(
+            e1) == "NamedEntity(name='one', reference='http://one.one', attributes=[BoundAttribute(name='concentration', value=Value(value=0.25, unit=Unit(reference='http://purl.obolibrary.org/obo/UO_0000064')))])"
+        assert str(
+            e1) == "NamedEntity(name='one', reference='http://one.one', attributes=[BoundAttribute(name='concentration', value=Value(value=0.25, unit=Unit(reference='http://purl.obolibrary.org/obo/UO_0000064')))])"
+
+    def test_entity_attribute_serialization(self):
+        concentration = Attribute.create_from(
+            name='concentration',
+            value=Value(
+                value=0.25,
+                unit=Unit(
+                    reference='http://purl.obolibrary.org/obo/UO_0000064'
+                )
+            ))
+        e1 = NamedEntity(
+            name="one",
+            reference="http://one.one",
+            attributes=[concentration])
+        e_json = json.dumps(e1, cls=NamedEntityEncoder)
+        e2 = json.loads(e_json, cls=NamedEntityDecoder)
+        assert e1 == e2
